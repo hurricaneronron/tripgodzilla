67096827b69c5e49a5d37a43e12db9db82968047
hurricaneronron <vprzybyl@gmail.com>
2018-06-20 22:34:17 -0700

    build right sidebar

--- client/src/components/Messages/CurrentMessages.js
+++ client/src/components/Messages/CurrentMessages.js
@@ -3,7 +3,8 @@ import React from "react";
 const CurrentMessages = props =>
 <div className="row">
     <div className="container">
-        <span>PROPS.USERNAME <a className="waves-effect waves-light red darken-4 btn-small">DELETE</a></span>
+        <p>PROPS.USERNAME</p>
+        <a className="waves-effect waves-light red darken-4 btn-small">DELETE</a>
     </div>
 </div>
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = props =>
   <div className="nav-wrapper blue darken-4">
     <a href="" className="brand-logo left">LOGO</a>
       <ul id="nav-mobile" className="right">
-      <li><a href="sass.html">Home</a></li>
-      <li><a href="badges.html">User</a></li>
-      <li><a href="collapsible.html">Trips</a></li>
-      <li><a href="collapsible.html">Messages</a></li>
+      <li><a href="/Home">Home</a></li>
+      <li><a href="/User">User</a></li>
+      <li><a href="/Trips">Trips</a></li>
+      <li><a href="/Messages">Messages</a></li>
     </ul>
   </div>
 </nav>
