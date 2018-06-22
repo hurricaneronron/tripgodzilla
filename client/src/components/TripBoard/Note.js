b171f2e1f1a8358287cbe935b60a59c4813378fc
hurricaneronron <vprzybyl@gmail.com>
2018-06-22 15:06:00 -0700

    build TripBoard page

---
+++ client/src/components/TripBoard/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = props =>
+<div className="card">
+    <div className="card-content">
+        <a className="waves-effect waves-light red darken-4 btn-small right" onClick="PROPS.handleDeleteNote">X</a>
+        <p>PROPS.DATE</p>
+        <p>PROPS.TRIPCOMMENT</p>
+    </div>
+</div>
+
+export default Note;
