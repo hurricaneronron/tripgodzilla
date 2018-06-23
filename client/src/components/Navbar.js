bb7ad747475a9f6e5502886a21ce8b2ba23ce5f9
hurricaneronron <vprzybyl@gmail.com>
2018-06-22 20:09:13 -0700

    logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import road from "../styles/images/road.svg";
+import sign from "../styles/images/sign.svg";
+import panel from "../styles/images/panel.svg";
+import "../styles/Navbar.css";
 
 const Navbar = props =>
 <nav>
   <div className="nav-wrapper blue darken-4">
-    <a href="" className="brand-logo left">LOGO</a>
+    <a href="" className="brand-logo left"><img id="logo" src={panel} />Brand Name</a>
       <ul id="nav-mobile" className="right">
       <li><a href="/Home">Home</a></li>
       <li><a href="/User">User</a></li>
