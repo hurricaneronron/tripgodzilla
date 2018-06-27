95dbc4a6960455deeb363bbbf99c51c293ce2c44
hurricaneronron <vprzybyl@gmail.com>
2018-06-26 18:59:33 -0700

    fix filter display in left sidebar

--- client/src/components/Home/LeftSidebar.js
+++ client/src/components/Home/LeftSidebar.js
@@ -1,18 +1,15 @@
 import React from "react";
 
 const LeftSidebar = props =>
-<div className="col s12 m2">
-    <div className="container">
-        <h6>Filters</h6>
-        <form action="#">
-            <p>
-                <label>
-                    <input type="checkbox" class="filled-in" />
-                    <span>{props.name}</span>
-                </label>
-            </p>
-        </form>
-    </div>
+<div className="container">
+    <form action="#">
+        <p>
+            <label>
+                <input type="checkbox" className="filled-in" />
+                <span>{props.name}</span>
+            </label>
+        </p>
+    </form>
 </div>
 
 export default LeftSidebar;
